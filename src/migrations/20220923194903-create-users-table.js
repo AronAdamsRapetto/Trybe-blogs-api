9d7c26149fd52805170143232c58c0fa9b903cad
Aron Adams <aronrapetto@hotmail.com>
2022-09-23 17:33:37 -0300

    feat: migration create-users-table implementado

---
+++ src/migrations/20220923194903-create-users-table.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      displayName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'display_name',
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+   await queryInterface.dropTable('users');
+  }
+};
